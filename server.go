2a7b59268d0b3981a3518831d501204437d87743
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-04 17:10:06 +0530

    feat: added API route to retrieve all pokemon records

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"goweb/db"
+	"goweb/models"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -9,12 +10,23 @@ import (
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello world!")
-	})
-	_, err := db.Connect()
+
+	dbInstance, err := db.Connect()
 	if err != nil {
 		e.Logger.Errorf("Error connecting to database: %v", err)
 	}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello world!")
+	})
+
+	e.GET("/api/v1/pokemons", func(c echo.Context) error {
+		var pokemons []models.Pokemon
+		if err := dbInstance.Find(&pokemons).Error; err != nil {
+			return c.String(http.StatusOK, "Something went wrong while quering the database")
+		}
+		return c.JSON(http.StatusOK, pokemons)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
