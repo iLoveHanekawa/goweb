c291b3a589c1765977e014eaf56d8217f9e724eb
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-04 15:55:26 +0530

    feat: database connection with db/connect.go and env setup

---
+++ db/connect.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func init() {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatal("error loading env file.", err)
+	}
+	log.Println("env successfully loaded.")
+}
+
+func Connect() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v sslmode=%v TimeZone=%v",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_DATABASE"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+		os.Getenv("DB_TIMEZONE"),
+	)
+	// dsn := "host=localhost user=postgres password=password dbname=gorm port=8080 sslmode=disable TimeZone=Asia/Kolkata"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db, err
+}
--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"goweb/db"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -11,5 +12,9 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello world!")
 	})
+	_, err := db.Connect()
+	if err != nil {
+		e.Logger.Errorf("Error connecting to database: %v", err)
+	}
 	e.Logger.Fatal(e.Start(":1323"))
 }
