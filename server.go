e47a79044a02b5e23edfadd581f497251644770d
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-05 17:11:41 +0530

    feat[restapi]: added rest router for querying with path params

--- server.go
+++ server.go
@@ -28,5 +28,14 @@ func main() {
 		return c.JSON(http.StatusOK, pokemons)
 	})
 
+	e.GET("/api/v1/pokemons/:id", func(c echo.Context) error {
+		var pokemon models.Pokemon
+		id := c.Param("id")
+		if result := dbInstance.First(&pokemon, id); result.Error != nil {
+			return c.String(http.StatusOK, "Something went wrong while quering the database")
+		}
+		return c.JSON(http.StatusOK, pokemon)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
