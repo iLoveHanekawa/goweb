1ce91e9fba0346ff00faa5e289bf4ee18d9d8d8e
iLoveHanekawa <arjunthakur900@gmail.com>
2024-03-05 21:27:34 +0530

    feat[ui]: added static navbar

--- views/index_templ.go
+++ views/index_templ.go
@@ -27,6 +27,10 @@ func Html() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
+		templ_7745c5c3_Err = Navbar().Render(ctx, templ_7745c5c3_Buffer)
+		if templ_7745c5c3_Err != nil {
+			return templ_7745c5c3_Err
+		}
 		templ_7745c5c3_Err = Home().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
